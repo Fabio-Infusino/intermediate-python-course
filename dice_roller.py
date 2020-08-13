a3e72162587c1934fa28a1111a6528d7cb9c25e4
Fabio-Infusino <69626138+Fabio-Infusino@users.noreply.github.com>
2020-08-13 12:19:11 +0200

    Update dice_roller.py
    Doppio 6 al primo tentativo!

--- dice_roller.py
+++ dice_roller.py
@@ -10,8 +10,13 @@ import random
 #creiamo una simulazione del lancio di due dadi dove verrà simulata e addizionata la scelta di due numeri casuali compresi tra 1 e 6
 dice_rolls = 2
 dice_sum = 0
-for i in range(0,dice_rolls):
-  roll = random.randint(1,6)
-  dice_sum += roll
-  print(f'Hai fatto {roll}')
-print(f'Hai fatto un totale di {dice_sum}')
+for i in range (0, dice_rolls):
+    roll = random.randint (1, 6)
+    dice_sum += roll
+    if roll == 1:
+        print (f'Hai tirato un {roll}! Risultato critico')
+    elif roll == 6:
+        print (f'Hai tirato un {roll}! Ottimo risultato!')
+    else:
+        print (f'Hai tirato un {roll}')
+    print(f'Hai tirato un totale di {dice_sum}')
