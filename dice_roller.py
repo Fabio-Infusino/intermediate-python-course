8b06a223a56ed6c7e95ed562d7de136e7d0b0530
Fabio-Infusino <69626138+Fabio-Infusino@users.noreply.github.com>
2020-08-13 12:28:28 +0200

    Update dice_roller.py
    tutorial completato

--- dice_roller.py
+++ dice_roller.py
@@ -7,16 +7,18 @@ if __name__== "__main__":
 #importiamo la funzione casuale
 import random
 
-#creiamo una simulazione del lancio di due dadi dove verrà simulata e addizionata la scelta di due numeri casuali compresi tra 1 e 6
-dice_rolls = 2
-dice_sum = 0
+#creiamo una simulazione del lancio di dadi dove verrà simulata e addizionata la scelta di due numeri casuali.
+import random
+dice_rolls  =  int (input('Quanti dadi vorresti tirate?')) #faccciamo scegliere al nostro utente quanti dadi vuole lanciare
+dice_size = int (input('Quante facce ha il tuo dado?')) #facciamo scegliere al nostro utente quante facce deve avere il dado
+dice_sum  =  0
 for i in range (0, dice_rolls):
-    roll = random.randint (1, 6)
+    roll = random.randint (1, dice_size)
     dice_sum += roll
     if roll == 1:
-        print (f'Hai tirato un {roll}! Risultato critico')
-    elif roll == 6:
-        print (f'Hai tirato un {roll}! Ottimo risultato!')
+        print (f'Hai tirato un {roll}! Risultato critico') #diremo che è un risultato critico quando esce il numero 1
+    elif roll == dice_size:
+        print (f'Hai tirato un {roll}! Ottimo risultato!') #quando il riusltato è il numero maggiore possibile scriveremo ottimo risultato
     else:
         print (f'Hai tirato un {roll}')
     print(f'Hai tirato un totale di {dice_sum}')
