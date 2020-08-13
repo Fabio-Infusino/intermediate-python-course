9ab2928d5cc80a9b2f33983acbbcc5431521590b
Fabio-Infusino <69626138+Fabio-Infusino@users.noreply.github.com>
2020-08-13 11:50:35 +0200

    Update dice_roller.py
    Abbiamo creato una funzione di scelta casuale di un numero per simulare il lancio di un dado

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,14 @@
 def main():
-  print('You rolled a die')
+  print('Lancia il dado')
 
 if __name__== "__main__":
   main()
+
+#importiamo la funzione casuale
+import random
+
+#roll sarà una scelta casuale dei numeri da 1 a 6
+roll = random.randint(1,6)
+
+#stampiamo il valore di roll
+print(f'Hai fatto {roll}!')
